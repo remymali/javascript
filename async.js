959d001160f9b1aae6a258bca94d844c1cf3e272
Remy M Ali <remy@peko.one>
2025-07-23 16:58:36 +0530

    async await with a reusable function

--- async.js
+++ async.js
@@ -1,33 +1,45 @@
 
 
-const promise1 = new Promise((resolve, reject) => {
-    // resolve(["Ayshu", "Remy"])
-    reject("api failed")
-})
-
-const promise2 = new Promise((resolve, reject) => {
-    resolve("second")
-})
-
-// promise1.then((res)=>{
-//     console.log(res)
-// }).catch((err)=>{
-//    console.log(err)
+// const promise1 = new Promise((resolve, reject) => {
+//     // resolve(["Ayshu", "Remy"])
+//     reject("api failed")
 // })
 
-const result = async () => {
-    try {
-        const pr = await promise1
-        const pr2 = await promise2
+// const promise2 = new Promise((resolve, reject) => {
+//     resolve("second")
+// })
 
-        console.log(pr + pr2)
+// // promise1.then((res)=>{
+// //     console.log(res)
+// // }).catch((err)=>{
+// //    console.log(err)
+// // })
 
-    } catch (error) {
+// const result = asynchandler(async () => {
+//     try {
+//         const pr = await promise1
+//         const pr2 = await promise2
 
-        console.log(error)
+//         console.log(pr + pr2)
 
-    }
+//     } catch (error) {
 
+//         console.log(error)
+
+//     }
+
+// })
+
+// result()
+
+const resolveJson=async(url)=>{
+    const val = await fetch(url)
+      const data=await val.json();
+      return data
 }
 
+const result=async()=>{
+const val = await resolveJson('https://jsonplaceholder.typicode.com/todos/1')
+      console.log(val)
+}
 result()
